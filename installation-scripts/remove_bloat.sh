cf33474589ddbe2450a72c15cf85134393a6fc5c
shama-llama <eyobgeletu@tutanota.com>
2025-06-12 21:15:46 +0300

    Add script to remove kde  bloatware

---
+++ installation-scripts/remove_bloat.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Remove bloatware installed by default by KDE
+echo "Removing KDE bloatware..."
+sudo dnf remove -y \
+    akregator \
+    dragon \
+    elisa-player \
+    hplip \
+    kaddressbook \
+    kdeconnectd \
+    korganizer \
+    kmahjongg \
+    kmail \
+    kmines \
+    kmouth \
+    kolourpaint \
+    kontact \
+    konversation \
+    kpat \
+    neochat
