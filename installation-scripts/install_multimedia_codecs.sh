ec2ba24d85d491115a66dda2ae583eb6e07a4afa
Shama Llama <eyobgeletu@tutanota.com>
2025-06-30 17:40:19 +0300

    Add hardware acceleration

--- installation-scripts/install_multimedia_codecs.sh
+++ installation-scripts/install_multimedia_codecs.sh
@@ -2,6 +2,9 @@
 
 echo "Starting multimedia support configuration..."
 
+echo "Installing multimedia packages..."
+sudo dnf group install Multimedia
+
 # Swap ffmpeg-free with ffmpeg
 echo "Swapping ffmpeg-free with ffmpeg..."
 sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
@@ -9,5 +12,8 @@ sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
 # Update multimedia packages
 echo "Updating multimedia packages..."
 sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+
+echo "Enabling hardware acceleration..."
+sudo dnf install intel-media-driver
 
 echo "Multimedia support configuration completed successfully!"
